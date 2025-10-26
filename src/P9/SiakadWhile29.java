be1b52aac62eab9e643947598fb840464e17809d
Fayyad <Alfayyad2106@gmail.com>
2025-10-27 00:35:25 +0700

    SiakadWhile29.java

---
+++ src/P9/SiakadWhile29.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SiakadWhile29 {
+public static void main(String[] args) {
+    Scanner sc29 = new Scanner(System.in);
+    int nilai, jml, i=0;
+
+    System.out.println("Masukkan jumlah siswa: ");
+    jml = sc29.nextInt();
+
+    while (i < jml) {
+        System.out.println("Masukkan jumlah mahasiswa ke-" + (i + 1) + ": ");
+        nilai = sc29.nextInt();
+
+        if (nilai < 0 || nilai > 100) {
+            System.out.println("nilai tidak valid. masukkan nilai yang valid!");
+            continue;
+        }   
+        
+        if (nilai > 80 && nilai <= 100) {
+            System.out.println("Nilai mahasiswa ke-" + (i+1) + "Adalah A");
+        } else if (nilai > 73 && nilai <= 80) {
+            System.out.println("Nilai mahasiswa ke-" + (i+1) + "Adalah B+");
+        } else if (nilai > 65 && nilai <= 73) {
+            System.out.println("Nilai mahasiswa ke-" + (i+1) + "Adalah B");
+        } else if (nilai > 60 && nilai <= 65) {
+            System.out.println("Nilai mahasiswa ke-" + (i+1) + "Adalah C+");
+        } else if (nilai > 50 && nilai <= 60) {
+            System.out.println("Nilai mahasiswa ke-" + (i+1) + "Adalah C");
+        } else {
+            System.out.println("Nilai mahasiswa ke-" + (i+1) + "Adalah E");
+        }
+
+        i++;
+    }
+}
+    
+}
