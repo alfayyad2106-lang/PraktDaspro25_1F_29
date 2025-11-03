6a99b356abec3a6df88373e1920c3d53d788be42
Fayyad <Alfayyad2106@gmail.com>
2025-11-03 21:24:40 +0700

    star29.java

---
+++ src/P10/star29.java
@@ -0,0 +1,17 @@
+package P10;
+
+import java.util.Scanner;
+
+public class star29 {
+
+    public static void main(String[] args) {
+        Scanner sultan29 = new Scanner(System.in);
+        System.out.println("Masukkan nilai n = ");
+        int n = sultan29.nextInt();
+
+        for (int i=1; i<=n; i++){
+            System.out.print("*");
+        }
+        sultan29.close();
+    }
+}
