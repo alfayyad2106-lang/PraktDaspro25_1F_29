1482e9ded9e581724d063501cbf319dcd34730af
Fayyad <Alfayyad2106@gmail.com>
2025-11-11 20:20:40 +0700

    ArrayNilai29.java

---
+++ src/P11/ArrayNilai29.java
@@ -0,0 +1,22 @@
+package P11;
+
+import java.util.Scanner;
+
+public class ArrayNilai29 {
+
+    public static void main(String[] args) {
+       Scanner sultan29 = new Scanner(System.in);
+       int[] nilaiAkhir = new int[10];
+       
+       for (int i = 0; i < 10; i++) {
+        System.out.print("Masukkan nilai akhir ke-" +i+" : ");
+        nilaiAkhir [i] = sultan29.nextInt();
+
+        
+       }
+
+       for (int i = 0; i < 10; i++) {
+        System.out.println("Nilai akhir ke-" +i+" adalah " +nilaiAkhir[i]);
+       }
+    }
+}
