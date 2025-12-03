adf888d678f455b6d18b7d4d8f80b20f45bf37a3
Fayyad <Alfayyad2106@gmail.com>
2025-12-03 09:30:54 +0700

    kafe29.java

---
+++ src/P13/Kafe29.java
@@ -0,0 +1,26 @@
+package P13;
+
+public class Kafe29 {
+    public static void Menu(){ 
+        // System.out.println("selamat datang, " +namaPelanggan29+ "!");
+        // if (isMember29) {
+        //     System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian");
+        // }
+
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. Kopi Hitam - RP 15,000");
+        System.out.println("2. Cappuccino - RP 20,000");
+        System.out.println("3. Latte - RP 22,000");
+        System.out.println("4. Teh Tarik - RP 12,000");
+        System.out.println("5. Roti Bakar - RP 10,000");
+        System.out.println("6. Mie Goreng - RP 18,000");
+        System.out.println("==========================");
+        System.out.println("Silahlkan pilih menu yang Anda inginkan");
+    }
+public static void main(String[] args) {
+        Menu( );
+    }
+       
+    
+    
+}
